1be703de232869382058b354641a8c9d3c0b3911
Tarek Noaman <tnoaman@kibkw.com>
2024-01-04 15:00:06 +0200

    chore: initial commit

---
+++ android/src/main/java/com/preventscreenmirroring/PreventScreenMirroringModule.java
@@ -0,0 +1,32 @@
+package com.preventscreenmirroring;
+
+import androidx.annotation.NonNull;
+
+import com.facebook.react.bridge.Promise;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.module.annotations.ReactModule;
+
+@ReactModule(name = PreventScreenMirroringModule.NAME)
+public class PreventScreenMirroringModule extends ReactContextBaseJavaModule {
+  public static final String NAME = "PreventScreenMirroring";
+
+  public PreventScreenMirroringModule(ReactApplicationContext reactContext) {
+    super(reactContext);
+  }
+
+  @Override
+  @NonNull
+  public String getName() {
+    return NAME;
+  }
+
+
+  // Example method
+  // See https://reactnative.dev/docs/native-modules-android
+  @ReactMethod
+  public void multiply(double a, double b, Promise promise) {
+    promise.resolve(a * b);
+  }
+}
---
+++ example/ios/File.swift
@@ -0,0 +1,6 @@
+//
+//  File.swift
+//  PreventScreenMirroringExample
+//
+
+import Foundation
